983dd7ae3612e77a0ff1232a276bd788d12e2432
Ryan DS <rdspring1@gmail.com>
2017-07-23 14:37:50 -0500

    Implemented Training Framework - Not Working

--- cnn.py
+++ cnn.py
@@ -1,4 +1,6 @@
 import os
+import numpy as np
+
 import torch
 import torchvision
 import torchvision.transforms as transforms
@@ -9,20 +11,22 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 import omniglot
+import memory
 
 class Net(nn.Module):
     def __init__(self, input_shape):
         super(Net, self).__init__()
-        ch, row, col = input_shape
+        # Constants
         kernel = 3
         pad = int((kernel-1)/2.0)
+
+        ch, row, col = input_shape
         self.conv1 = nn.Conv2d(ch, 64, kernel, padding=(pad, pad))
         self.conv2 = nn.Conv2d(64, 64, kernel, padding=(pad, pad))
         self.conv3 = nn.Conv2d(64, 128, kernel, padding=(pad, pad))
         self.conv4 = nn.Conv2d(128, 128, kernel, padding=(pad, pad))
         self.pool = nn.MaxPool2d(2, 2)
         self.fc1 = nn.Linear(row // 4 * col // 4 * 128, 128)
-        self.softmax = nn.Linear(128, classes)
 
     def forward(self, x):
         x = F.relu(self.conv1(x))
@@ -35,6 +39,8 @@ class Net(nn.Module):
         x = self.fc1(x)
         return x
 
+memory_size = 8192
+key_dim = 128
 episode_length = 30
 episode_width = 5
 DATA_FILE_FORMAT = os.path.join(os.getcwd(), '%s_omni.pkl')
@@ -45,42 +51,49 @@ trainloader = trainset.sample_episode_batch(episode_length, episode_width, batch
 
 test_filepath = DATA_FILE_FORMAT % 'test'
 testset = omniglot.OmniglotDataset(test_filepath)
-testloader = testset.sample_episode_batch(episode_length, episode_width, batch_size=1, N=50)
 
-'''
 #torch.cuda.set_device(1)
 net = Net(input_shape=(1,28,28))
+mem = memory.Memory(memory_size, key_dim)
 net.cuda()
+
+optimizer = optim.Adam(net.parameters(), lr=1e-4, eps=1e-4)
 
 for i, data in enumerate(trainloader, 0):
     # training
-    inputs, labels = data
-    for x, y in zip(inputs, labels):
-        x, y = Variable(x.cuda()), Variable(labels.cuda())
+    x, y = data
+    for xx, yy in zip(x, y):
+        xx_cuda, yy_cuda = Variable(xx.cuda()), Variable(yy.cuda())
         optimizer.zero_grad()
-        y_hat = net(x)
-        loss = criterion(y_hat, y)
+        query = net(xx_cuda)
+        yy_hat, embed, loss = mem.query(query, yy_cuda, False)
         loss.backward()
         optimizer.step()
 
     correct = []
     correct_by_k_shot = dict((k, list()) for k in range(episode_width + 1))
+    testloader = testset.sample_episode_batch(episode_length, episode_width, batch_size=1, N=50)
     for i, data in enumerate(testloader, 0):
+        # validation
         x, y = data
-        x, y = Variable(x.cuda())
-        y_hat = net(x)
-        correct.append(torch.mean(torch.eq(y, y_hat)))
+        y_hat = []
+        for xx, yy in zip(x, y):
+            xx_cuda, yy_cuda = Variable(xx.cuda()), Variable(yy.cuda())
+            query = net(xx_cuda)
+            yy_hat, embed, loss = mem.query(query, yy_cuda, False)
+            correct.append(float(torch.equal(yy, yy_hat)))
+            y_hat.append(yy_hat)
 
         # compute per_shot accuracies
-        seen_count = [[0] * episode_width]
+        seen_count = [0 for idx in range(episode_width)]
         # loop over episode steps
         for yy, yy_hat in zip(y, y_hat):
-            count = seen_count[yy % episode_width]
+            yy_value = yy[0]
+            count = seen_count[yy_value % episode_width]
             if count < (episode_width + 1):
-                correct_by_k_shot[count].append(torch.eq(yy, yy_hat))
-            seen_count[yy % episode_width] += 1
+                correct_by_k_shot[count].append(float(torch.equal(yy, yy_hat)))
+            seen_count[yy_value % episode_width] += 1
 
     print("validation overall accuracy {0:f}".format(np.mean(correct)))
     for idx in range(episode_width + 1):
         print("{0:d}-shot: {1:.3f}".format(idx, np.mean(correct_by_k_shot[idx])))
-'''
--- memory.py
+++ memory.py
@@ -1,18 +1,40 @@
 import torch
 import torch.autograd as ag
 import torch.nn as nn
+import torch.nn.functional as F
 import numpy as np
+import math
+import functools
 
 def random_uniform(shape, low, high):
     x = torch.rand(*shape)
-    return (high - low) * x + low
+    result = (high - low) * x + low
+    return result
 
-def normalize(matrix, dim):
-    """ Normalize Matrix - Unit Norm """
-    total = torch.sum(matrix, dim)
-	return matrix / total
+def multiply(x):
+    return functools.reduce(lambda x,y: x*y, x, 1)
 
-# TODO Memory Loss Function
+def flatten(x):
+    """ Flatten matrix into a vector """
+    count = multiply(x.size())
+    return x.resize_(count)
+
+def index(batch_size, x):
+    idx = torch.arange(0, batch_size).long() 
+    idx = torch.unsqueeze(idx, -1)
+    return torch.cat((idx, x), dim=1)
+
+def MemoryLoss(positive, negative, margin):
+    """
+        Calculate Average Memory Loss Function
+        positive - positive cosine similarity
+        negative - negative cosine similarity
+        margin
+    """
+    assert(positive.size() == negative.size())
+    dist_hinge = torch.clamp(negative - positive + margin, min=0.0)
+    loss = torch.mean(dist_hinge)
+    return loss
 
 """
 Softmax Temperature -
@@ -23,68 +45,119 @@ Softmax Temperature -
 
 class Memory(nn.Module):
     def __init__(self, memory_size, key_dim, top_k = 256, inverse_temp = 40, age_noise=8.0, margin = 0.1):
-        #Initialize normalized key matrix
-        self.keys = nn.Parameter(normalize(torch.randn(memory_size, key_dim), dim=1))
-        self.value = nn.Parameter(torch.zeros(memory_size, 1))
-        self.age = nn.Parameter(torch.zeros(memory_size, 1))
+        super(Memory, self).__init__()
+        self.keys = F.normalize(torch.randn(memory_size, key_dim), dim=1)
+        self.values = torch.zeros(memory_size, 1).long()
+        self.age = torch.zeros(memory_size, 1)
 
         self.memory_size = memory_size
         self.key_dim = key_dim
         self.top_k = min(top_k, memory_size)
-        self.softmax_temp = max(1.0, torch.log(0.2 * topk) / inverse_temp)
+        self.softmax_temperature = max(1.0, math.log(0.2 * top_k) / inverse_temp)
         self.age_noise = age_noise
         self.margin = margin
 
-    def forward(self, input, labels):
+    def predict(self, x):
+        query = F.normalize(x, dim=1)
+        keys_var = ag.Variable(self.keys, requires_grad=False)
+        batch_size, dims = query.size()
+
+        # Find the k-nearest neighbors of the query
+        scores = torch.matmul(query, torch.t(keys_var))
+        cosine_similarity, topk_indices_var = torch.topk(scores, self.top_k, dim=1)
+
+        # retrive memory values - prediction
+        y_hat_indices = topk_indices_var.data[:, 0]
+        y_hat = self.values[y_hat_indices]
+
+        # softmax of cosine similarities - embedding
+        softmax_score = F.softmax(self.softmax_temperature * cosine_similarity)
+        return y_hat, softmax_score
+
+    def query(self, x, y, predict=False):
         """
         Compute the nearest neighbor of the input queries.
 
         Arguments:
-            input: A normalized matrix of queries of size (batch_size x key_dim)
+            x: A normalized matrix of queries of size (batch_size x key_dim)
+            y: A matrix of correct labels (batch_size x 1)
         Returns:
-            result, A (batch-size x 1) matrix 
-		- the nearest neighbor to the query in memory_size
+            y_hat, A (batch-size x 1) matrix 
+		        - the nearest neighbor to the query in memory_size
             softmax_score, A (batch_size x 1) matrix 
-		- A normalized score measuring the similarity between query and nearest neighbor
+		        - A normalized score measuring the similarity between query and nearest neighbor
+            loss - average loss for memory module
         """
-        normalized_input = normalize(input, dim=1)
+        x, y = x.cpu(), y.cpu()
+        query = F.normalize(x, dim=1)
+        keys_var = ag.Variable(self.keys, requires_grad=False)
+        batch_size, dims = query.size()
 
         # Find the k-nearest neighbors of the query
-        scores = torch.matmul(normalized_input, torch.t(self.keys.data))
-        values, indices = torch.topk(scores, self.top_k, dim = 1)
-        result = self.value[indices[:, 0]]
+        scores = torch.matmul(query, torch.t(keys_var))
+        cosine_similarity, topk_indices_var = torch.topk(scores, self.top_k, dim=1)
 
-        # Calculate similarity values
-        cosine_similarity = torch.dot(normalized_input, torch.t(self.keys[indices, :]))
-        softmax_score = nn.Softmax(self.inverse_temperature * cosine_similarity)
+        topk_indices = topk_indices_var.detach().data
+        y_hat_indices = topk_indices[:, 0]
+        y_hat = self.values[y_hat_indices]
+
+        softmax_score = F.softmax(self.softmax_temperature * cosine_similarity)
+
+        loss = None
+        if not predict:
+            # Loss Function
+            # topk_indices = (batch_size x topk)
+            # topk_values =  (batch_size x topk x value_size)
+
+            # collect the memory values corresponding to the topk scores
+            flat_topk = flatten(topk_indices)
+            flat_topk_values = self.values[topk_indices]
+            topk_values = flat_topk_values.resize_(batch_size, self.top_k)
+
+            correct_mask = torch.eq(topk_values, torch.unsqueeze(y.data, dim=1)).float()
+            correct_mask_var = ag.Variable(correct_mask, requires_grad=False)
+
+            pos_score, pos_idx = torch.topk(torch.mul(cosine_similarity, correct_mask_var), 1, dim=1)
+            neg_score, neg_idx = torch.topk(torch.mul(cosine_similarity, 1-correct_mask_var), 1, dim=1)
+
+            loss = MemoryLoss(pos_score, neg_score, self.margin)
 
         # Update memory
-        self.update(normalize_input, labels, indices) 
+        self.update(query, y, y_hat, y_hat_indices) 
 
-        return result, softmax_score
+        return y_hat, softmax_score, loss
 
-    def update(query, y, y_hat):
+    def update(self, query, y, y_hat, y_hat_indices):
+        batch_size, dims = query.size()
+
         # 1) Untouched: Increment memory by 1
-        self.age = torch.add(self.age, 1)
+        self.age += 1
 
-        # indices
-        correct = torch.eq(y, y_hat)
-        correct_indices = torch.nonzero(correct)
-        incorrect_indices = torch.nonzero(1-correct)
+        # Divide batch by correctness
+        result = torch.squeeze(torch.eq(y_hat, torch.unsqueeze(y.data, dim=1))).float()
+        incorrect_examples = torch.squeeze(torch.nonzero(1-result))
+        correct_examples = torch.squeeze(torch.nonzero(result))
+
+        incorrect = len(incorrect_examples.size()) > 0
+        correct = len(correct_examples.size()) > 0
 
         # 2) Correct: if V[n1] = v
         # Update Key k[n1] <- normalize(q + K[n1]), Reset Age A[n1] <- 0
-        correct_values = self.value[correct_indices]
-        correct_query = query[correct_indices]
-        update_correct_values = normalize(correct_values + correct_query, dim=1)
-        self.age[correct_indices] = 0
+        if correct:
+            correct_indices = y_hat_indices[correct_examples]
+            correct_keys = self.keys[correct_indices]
+            correct_query = query.data[correct_examples]
+            new_correct_keys = F.normalize(correct_keys + correct_query, dim=1)
+            self.keys[correct_indices] = new_correct_keys
+            self.age[correct_indices] = 0
 
         # 3) Incorrect: if V[n1] != v
         # Select item with oldest age, Add random offset - n' = argmax_i(A[i]) + r_i 
         # K[n'] <- q, V[n'] <- v, A[n'] <- 0
-        age_with_noise = self.age + random_uniform((self.memory_size, 1), -self.age_noise, self.age_noise)
-        valuse, oldest_indices = torch.topk(age_with_noise, self.batch_size, dim = 0)
-        self.keys[oldest_indices] = query
-        self.values[oldest_indices] = y
-        self.age[oldest_indices] = 0
-
+        if incorrect:
+            age_with_noise = self.age + random_uniform((self.memory_size, 1), -self.age_noise, self.age_noise)
+            topk_values, topk_indices = torch.topk(age_with_noise, batch_size, dim=0)
+            oldest_indices = torch.squeeze(topk_indices)
+            self.keys[oldest_indices] = query.data
+            self.values[oldest_indices] = torch.unsqueeze(y.data, dim=1)
+            self.age[oldest_indices] = 0
